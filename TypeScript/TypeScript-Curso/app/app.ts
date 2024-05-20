5d85c50542704e9cc404f31da9f4793deba7fdcb
LeoCarrijo <lh83712@gmail.com>
2024-05-20 09:33:55 -0300

    :recycle: refactor: Tornando Classe 'View' abstrata

--- TypeScript/TypeScript-Curso/app/app.ts
+++ TypeScript/TypeScript-Curso/app/app.ts
@@ -1,5 +1,6 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js"; // Colocar .js no final do arquivo importado
 import { NegociacoesView } from "./views/negociacoes-view.js";
+import { View } from "./views/view.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
--- TypeScript/TypeScript-Curso/dist/js/views/view.js
+++ TypeScript/TypeScript-Curso/dist/js/views/view.js
@@ -2,9 +2,6 @@ export class View {
     constructor(seletor) {
         this._elemento = document.querySelector(seletor);
     }
-    template(model) {
-        throw Error("Classe filha precisa implementar o método");
-    }
     update(model) {
         const template = this.template(model);
         this._elemento.innerHTML = template;
